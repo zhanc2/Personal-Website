dbdaccb9d2e65bec230ffda1999c7c1e81cc05a4
zhanc2 <76535472+zhanc2@users.noreply.github.com>
2023-08-04 15:35:10 -0400

    Change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as HashRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/Navbar';
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
